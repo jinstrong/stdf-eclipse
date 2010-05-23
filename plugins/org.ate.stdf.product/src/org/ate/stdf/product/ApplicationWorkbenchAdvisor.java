7cfeb552b90e418d22f9b4668adb5d37e217c32e
Krasimir.K <Krasimir.K@5e98db73-bfb4-f4e3-f028-eeb4f25e2fc2>
2010-05-23 17:59:19 +0000

    Initial import.

---
+++ plugins/org.ate.stdf.product/src/org/ate/stdf/product/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package org.ate.stdf.product;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "org.ate.stdf.product.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
